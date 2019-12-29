817577a48ffbd5908b6974edbd06dcc1939e3780
scshark <465841993@qq.com>
2019-12-29 18:16:04 +0800

    acc

--- app/Http/Middleware/AccessControlAllowOrigin.php
+++ app/Http/Middleware/AccessControlAllowOrigin.php
@@ -21,7 +21,7 @@ class AccessControlAllowOrigin
         header('Access-Control-Allow-Origin: *');
         header("Access-Control-Allow-Credentials: true");
         header("Access-Control-Allow-Methods: *");
-        header("Access-Control-Allow-Headers: Content-Type,Access-Token");
+        header("Access-Control-Allow-Headers: *");
         header("Access-Control-Expose-Headers: *");
 
         return $next($request);
